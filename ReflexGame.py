486c64ba6b940c2e4714f63286cbed5ec4e18448
adityakp15 <aditponnu@gmail.com>
2020-10-16 19:03:35 +0530

    First Commit

---
+++ ReflexGame.py
@@ -0,0 +1,40 @@
+#initialising pygame
+
+import pygame
+import sys
+import time
+from utils import *
+pygame.init()
+
+screen = pygame.display.set_mode((width,height))
+
+def drawGrid():
+    blockSize = 20 #Set the size of the grid block
+    for x in range(20):
+        for y in range(20):
+            rect = pygame.Rect(x*blockSize, y*blockSize,blockSize, blockSize)
+            pygame.draw.rect(screen,WHITE,rect, 1)
+            
+def main():
+    #initialising display 
+
+    background = GRAY
+    screen.fill(background)
+    # pygame.display.update()
+
+    #creating event loop
+    running = True
+    while running:
+        drawGrid()
+        for event in pygame.event.get():
+            # print(event)
+            if(event.type == pygame.QUIT):
+                # screen.fill(RED)
+                # pygame.display.update()
+                # time.sleep(1)
+                running = False
+        pygame.display.update()
+    pygame.quit()
+    quit()
+
+main()
