a8b8dfd3066e05ffe5a2a1d5e216428c221f4a04
adityakp15 <aditponnu@gmail.com>
2020-10-18 15:45:30 +0530

    Centered grid

--- ReflexGame.py
+++ ReflexGame.py
@@ -10,9 +10,11 @@ screen = pygame.display.set_mode((width,height))
 
 def drawGrid():
     blockSize = 20 #Set the size of the grid block
+    grid_width = pow(blockSize,2)
+    centering = (width - grid_width)//2
     for x in range(20):
         for y in range(20):
-            rect = pygame.Rect(x*blockSize, y*blockSize,blockSize, blockSize)
+            rect = pygame.Rect(x*blockSize + centering, y*blockSize + (centering-40),blockSize, blockSize)
             pygame.draw.rect(screen,WHITE,rect, 1)
             
 def main():
